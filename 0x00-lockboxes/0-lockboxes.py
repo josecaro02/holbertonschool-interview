db5cbee78860e11449cb27bacb646b9e7115b288
Jose David Caro <j.caro0207@gmail.com>
2021-01-28 18:59:52 -0500

    Add task

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    ''' Function to unlock boxes '''
+    lenBoxes = len(boxes)
+    openedBoxes = [1]
+    keys = []
+    for i in range(1, lenBoxes):
+        openedBoxes.append(0)
+    for box in boxes[0]:
+        keys.append(box)
+        openedBoxes[box] = 1
+    posBox = 1
+    while posBox < lenBoxes:
+        if posBox in keys:
+            for key in boxes[posBox]:
+                keys.append(key)
+                if openedBoxes[key] == 0 and key < posBox:
+                    openedBoxes[key] = 1
+                    posBox = key - 1
+                    break
+                openedBoxes[key] = 1
+        posBox += 1
+    if 0 in openedBoxes:
+        return False
+    return True
