4672ce13778a6337b7d599fb0ccb6a6fed99249b
Jose David Caro <j.caro0207@gmail.com>
2021-02-10 13:25:19 -0500

    [Fix betty and add header file]

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -14,13 +14,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new;
 
 	new = malloc(sizeof(binary_tree_t));
-	if(new == NULL)
-		return(NULL);
+	if (new == NULL)
+		return (NULL);
 
 	new->n = value;
 	new->left = NULL;
 	new->right = NULL;
 	new->parent = parent;
 
-	return new;
+	return (new);
 }
