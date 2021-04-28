15ad252955664e4d66106f84f24316d37316f72f
Jose David Caro <j.caro0207@gmail.com>
2021-04-28 01:41:33 -0500

    [Add conditional]

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -18,7 +18,7 @@ listint_t *insert_node(listint_t **head, int number)
 	if (new == NULL)
 		return (NULL);
 	new->n = number;
-	if (number <= current->n)
+	if (number <= current->n || current == NULL)
 	{
 		new->next = current;
 		*head = new;
