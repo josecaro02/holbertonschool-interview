2f4bc0a7df8181897b71d9593ba73e4ba9717a15
Jose David Caro <j.caro0207@gmail.com>
2021-05-12 00:33:59 -0500

    Fix pep8 and test new feature

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,18 +4,19 @@
 Minimum operations function
 """
 
+
 def minOperations(n):
+    if n <= 0:
+        return 0
     actualNum = 1
     copyNum = 0
     numOperations = 0
     while n != actualNum:
-        if n % actualNum == 0:                                                                                                                              
+        if n % actualNum == 0:
             numOperations += 2
             copyNum = actualNum
             actualNum += copyNum
         elif n % actualNum != 0:
             numOperations += 1
-            actualNum += copyNum                                                                                                                                 
-    return numOperations                                                                                                                                                      
-                                                                                                                                                      
-               
+            actualNum += copyNum
+    return numOperations
