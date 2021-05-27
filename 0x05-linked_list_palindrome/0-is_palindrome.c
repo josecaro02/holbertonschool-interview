dbade0eb3b24ef0edf041352d1bf471a6a67086e
Jose David Caro <j.caro0207@gmail.com>
2021-05-27 11:00:32 -0500

    [change variables]

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,38 +1,40 @@
 #include "lists.h"
+
 /**
- * recurPalindrome - checks if singly linked list is palindrome
- * @left: pointer to head of singly linked list
- * @right: head of singly linked list
+ * is_palindrome - function to check if a ll is palindrome
+ *@head: head of the linked list
  *
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ *Return: 1 if palindrome 0 otherwise
  */
 
-int recurPalindrome(listint_t **left, listint_t *right)
-{
-	int ret;
-
-	if (right == NULL)
-		return (1);
-
-	ret = recurPalindrome(left, right->next);
-	if (ret == 0)
-		return (0);
-
-	ret = (right->n == (*left)->n);
-
-	*left = (*left)->next;
-
-	return (ret);
-}
-/**
- * is_palindrome - checks if singly linked list is palindrome
- * @head: pointer to head of singly linked list
- *
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- */
 int is_palindrome(listint_t **head)
 {
-	if (!head)
+	listint_t *tail, *current;
+	int n_nodes, i, j;
+
+	n_nodes = 1;
+	tail = current = *head;
+	if (current == NULL)
 		return (0);
-	return (recurPalindrome(head, *head));
+	if (current->next == NULL)
+		return  (1);
+	while (tail->next != NULL)
+	{
+		n_nodes++;
+		tail = tail->next;
+	}
+
+	tail = *head;
+	for (i = 0; i < n_nodes / 2; i++)
+	{
+		for (j = 0; j < n_nodes - i - 1; j++)
+			tail = tail->next;
+		if (current->n != tail->n)
+			return (0);
+		current = current->next;
+		tail = *head;
+	}
+
+	return (1);
 }
+
