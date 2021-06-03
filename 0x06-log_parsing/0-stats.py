fb7d20e9203e191642ba4655bd8a379a48366580
Jose David Caro <j.caro0207@gmail.com>
2021-06-03 10:45:23 -0500

    Add task project

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import sys
+
+status = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+
+count = 0
+fileSize = 0
+try:
+    for line in sys.stdin:
+        splitLine = line.split()
+        fileSize += int(splitLine[8])
+        status[splitLine[7]] += 1
+        count += 1
+        if count == 10:
+            print("File size: {}".format(fileSize))
+            for key, value in status.items():
+                print("{}: {}".format(key, value))
+            count = 0
+except KeyboardInterrupt:
+    print("File size: {}".format(fileSize))
+    for key, value in status.items():
+        print("{}: {}".format(key, value))
---
+++ 0x06-log_parsing/README.md
@@ -0,0 +1,1 @@
+# README log parsing project
