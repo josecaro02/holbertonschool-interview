685d7eda4eed259cc84a3ce93abf52677bd5d197
Jose David Caro <j.caro0207@gmail.com>
2021-07-29 23:00:29 -0500

    Fix header file

--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -1,3 +1,7 @@
+
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+#include <stdlib.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -15,3 +19,8 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
+avl_t *sorted_array_to_avl(int *array, size_t size);
+void binary_tree_print(const binary_tree_t *);
+
+#endif /*BINARY_TREES_H*/
