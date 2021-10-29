ccdca860e20bad59fa49677f35471fa01d5a5a1f
Jose David Caro <j.caro0207@gmail.com>
2021-10-28 20:39:38 -0500

    Add task

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    placeholder = total + 1
+
+    memo = {0: 0}
+
+    for i in range(1, total + 1):
+        memo[i] = placeholder
+
+        for coin in coins:
+            current = i - coin
+            if current < 0:
+                continue
+
+            memo[i] = min(memo[current] + 1, memo[i])
+
+    if memo[total] == total + 1:
+        return -1
+
+    return memo[total]
---
+++ 0x19-making_change/REAMDE.md
@@ -0,0 +1,1 @@
+# README making change project
